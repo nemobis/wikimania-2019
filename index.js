4622956577b5cb8216b35c6cc49765b2c5b5df28
maxlath <k@maxlath.eu>
2018-11-27 11:33:17 -0800

    debugging

--- index.js
+++ index.js
@@ -3,28 +3,40 @@ const filename = document.location.pathname.split('/').slice(-1)[0]
 const filenameNumericId = parseInt(filename.split('.')[0])
 const lastPageNumericId = 15
 
-const previousPage = () => {
-  const previousPageNumericId = filenameNumericId - 1
-  if (previousPageNumericId < 1) return
-  navigatePage(previousPageNumericId)
-}
+document.addEventListener('DOMContentLoaded', event => {
+  console.log('READY')
+  const previousPage = () => {
+    console.log('previousPage')
+    const previousPageNumericId = filenameNumericId - 1
+    if (previousPageNumericId < 1) return
+    navigatePage(previousPageNumericId)
+  }
 
-const nextPage = () => {
-  const nextPageNumericId = filenameNumericId + 1
-  if (nextPageNumericId > lastPageNumericId) return
-  navigatePage(nextPageNumericId)
-}
+  const nextPage = () => {
+    console.log('nextPage')
+    const nextPageNumericId = filenameNumericId + 1
+    if (nextPageNumericId > lastPageNumericId) return
+    navigatePage(nextPageNumericId)
+  }
 
-const navigatePage = pageNumericId => {
-  document.location.href = `${pagesRoot}/${pageNumericId}.html`
-}
+  const navigatePage = pageNumericId => {
+    console.log('navigatePage', pageNumericId)
+    document.location.href = `${pagesRoot}/${pageNumericId}.html`
+  }
 
-const actionKeys = {
-  ArrowLeft: previousPage,
-  ArrowRight: nextPage
-}
+  const actionKeys = {
+    ArrowLeft: previousPage,
+    ArrowRight: nextPage
+  }
 
-document.addEventListener('keydown', event => {
-  const { key } = event
-  if (actionKeys[key]) actionKeys[key]()
+  const onKeydown = event => {
+    console.log('onKeydown', event)
+    const { key } = event
+    console.log('event', event)
+    console.log('key', key)
+    if (actionKeys[key]) actionKeys[key]()
+  }
+
+  window.onKeydown = onKeydown
+  document.addEventListener('keydown', onKeydown)
 })
