0141a499b5638c226ea44aed9122774bdbd8bf04
maxlath <k@maxlath.eu>
2018-12-15 17:09:09 +0100

    index: cleaning

--- index.js
+++ index.js
@@ -4,23 +4,19 @@ const filenameNumericId = parseInt(filename.split('.')[0])
 const lastPageNumericId = 15
 
 document.addEventListener('DOMContentLoaded', event => {
-  console.log('READY')
   const previousPage = () => {
-    console.log('previousPage')
     const previousPageNumericId = filenameNumericId - 1
     if (previousPageNumericId < 1) return
     navigatePage(previousPageNumericId)
   }
 
   const nextPage = () => {
-    console.log('nextPage')
     const nextPageNumericId = filenameNumericId + 1
     if (nextPageNumericId > lastPageNumericId) return
     navigatePage(nextPageNumericId)
   }
 
   const navigatePage = pageNumericId => {
-    console.log('navigatePage', pageNumericId)
     document.location.href = `${pagesRoot}/${pageNumericId}.html`
   }
 
@@ -30,10 +26,7 @@ document.addEventListener('DOMContentLoaded', event => {
   }
 
   const onKeydown = event => {
-    console.log('onKeydown', event)
     const { key } = event
-    console.log('event', event)
-    console.log('key', key)
     if (actionKeys[key]) actionKeys[key]()
   }
 
